286afe90738abdbaa44173be72cbecf7d8b971bf
ryzen <iron4540@gmail.com>
2024-06-03 22:14:37 +0530

    change

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage {
     uint public tokenCount;
-    constructor() ERC721("DApp NFT", "DAPP"){}
-    function mint(string memory _tokenURI) external returns(uint) {
-        tokenCount ++;
+    constructor() ERC721("XP NFT", "XP") {}
+    function mint(string memory _tokenURI) external returns (uint) {
+        tokenCount++;
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
-        return(tokenCount);
+        return (tokenCount);
     }
 }
